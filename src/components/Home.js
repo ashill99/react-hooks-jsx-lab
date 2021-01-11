c723f9298465117a9b89d9a8bb35dee8454e48d1
ashill99 <admshng@gmail.com>
2021-01-11 15:16:14 -0500

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+let style1 = {
+  color: 'firebrick'
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div>Home
+    <h1 style={style1} id="home"> {name} is a Web Developer from {city} </h1>
+  </div>;
 }
 
 export default Home;
